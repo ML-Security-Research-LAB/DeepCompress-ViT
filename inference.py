74bc33c42de35c4f817e2dc1f222cd9003d32608
sabbiracoustic1006 <sabbiracoustic1006@gmail.com>
2025-05-26 13:27:02 -0400

    py files added

---
+++ inference.py
@@ -0,0 +1,108 @@
+import os
+import timm
+import torch
+import argparse
+from tqdm import tqdm
+from src.models import get_models, get_encoder_decoders
+from src.utils import (seed_everything, get_dataloaders_imagenet, compare_model_parameters, evaluate_new_mixed, 
+                       get_layer_names, update_model_weights, process_model_blocks, create_model_for_flops, count_flops,
+                       analyze_parameter_storage, analyze_parameter_storage_mask_rcnn_vit_backbone)
+
+
+# path = '/home/sahmed9/codes/ViT-Compression-Latest/saved_models/small_all_layers_qvit_quant/deit_small_patch16_224.pth'
+
+def change_to_half(compressed_model):
+    # Iterate through all modules in the model
+    for module in compressed_model.modules():
+        if isinstance(module, torch.nn.LayerNorm):
+            if module.weight is not None:
+                module.weight.data = module.weight.data.half()
+            if module.bias is not None:
+                module.bias.data = module.bias.data.half()
+
+    # Convert pos_embed to half if it exists in the model
+    if hasattr(compressed_model, 'pos_embed'):
+        compressed_model.pos_embed.data = compressed_model.pos_embed.data.half()
+        
+    if hasattr(compressed_model, 'cls_token'):
+        compressed_model.cls_token.data = compressed_model.cls_token.data.half()
+    
+    # Convert all bias parameters to half precision
+    for name, param in compressed_model.named_parameters():
+        if 'bias' in name and param is not None:
+            param.data = param.data.half()
+
+def main(args):
+    seed_everything()
+    torch.cuda.set_device(args.device)
+    
+    state = torch.load(args.state_path, map_location=args.device)
+
+    args.hidden_dim = 768 if 'base' in args.model_name else 384
+
+    # Load and prepare the model
+    model, compressed_model = get_models(args)
+
+    selected_layers = get_layer_names(args)
+
+    # extract original weights and input sequence lengths (total batch of weights)
+    original_weights, input_seq_lens = process_model_blocks(model, compressed_model, args, selected_layers=selected_layers, 
+                                                            skip_qkv=args.skip_qkv)
+
+    # Get data loaders
+    _, val_loader = get_dataloaders_imagenet(args.batch_size)
+
+    # FLOPs calculation
+    print('Original model:')
+    count_flops(model, torch.rand(1,3,224,224).cuda())
+    model_flops = create_model_for_flops(model, args, selected_layers) 
+           
+    print('Compressed model:')
+    count_flops(model_flops, torch.rand(1,3,224,224).cuda())
+    del model_flops
+    
+    global encoders, decoders
+
+    encoders, decoders = get_encoder_decoders(selected_layers, input_seq_lens, args)
+    
+    for key in encoders.keys():
+        print(f'Encoder {key}: {encoders[key]}')
+        encoders[key].load_state_dict(state['encoder_states'][key])
+        decoders[key].load_state_dict(state['decoder_states'][key])
+        
+    compressed_model.load_state_dict(state['model_state_dict'])
+    change_to_half(compressed_model)
+
+    compare_model_parameters(model, compressed_model, encoders, 
+                             decoders, original_weights, args)
+    
+    analyze_parameter_storage(compressed_model, encoders, decoders, original_weights, args)
+    
+    if args.mask_rcnn_backbone:
+        print('Analyzing parameter storage for Mask R-CNN backbone...')
+        analyze_parameter_storage_mask_rcnn_vit_backbone(compressed_model, encoders, decoders, original_weights, args)
+    
+    decoded_weights = {}
+    for key in decoders.keys():
+        decoded_weights[key] = decoders[key](encoders[key](original_weights[key].to(args.device)))
+    
+    update_model_weights(compressed_model, decoded_weights, total_blocks=args.total_blocks, selected_layers=selected_layers,
+                         skip_qkv=args.skip_qkv, hidden_dim=args.hidden_dim)
+
+    best_acc = evaluate_new_mixed(val_loader, compressed_model, iters=-1, mp=args.mixed_precision)
+
+    print(f'Best accuracy: {best_acc:.2f}%')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train Vision Transformer with weight compression')
+    parser.add_argument('--model_name', type=str, default='deit_small_patch16_224', help='Name of the Vision Transformer model')
+    parser.add_argument('--total_blocks', type=int, default=12, help='Number of blocks to compress')
+    parser.add_argument('--device', type=str, default='cuda:0', help='Device to use for training')
+    parser.add_argument('--mixed_precision', action='store_true', help='Use mixed precision training')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size for ImageNet dataloader')
+    parser.add_argument('--state_path', type=str, default='saved_models/small_rank_277/deit_small_patch16_224.pth', help='Path to the model state dict')
+    parser.add_argument('--skip_qkv', action='store_true', help='Skip compressing qkv layer in MultiheadAttention')
+    parser.add_argument('--rank', type=int, default=200, help='Rank for encoded weight matrices')
+    parser.add_argument('--mask-rcnn-backbone', action='store_true', help='Show storage for Mask R-CNN backbone')
+    args = parser.parse_args()
+    main(args)
---
+++ main.py
@@ -0,0 +1,104 @@
+import os
+import timm
+import torch
+import argparse
+from tqdm import tqdm
+from src.models import get_models, get_encoder_decoders
+from src.utils import (seed_everything, get_dataloaders_imagenet, get_layer_names, create_optimizer_scheduler, 
+                       process_model_blocks, analyze_parameter_storage)
+from src.training import initial_fit, train
+
+
+def main(args):
+    seed_everything()
+    os.makedirs(f'saved_models/{args.base_dir}', exist_ok=True)
+    torch.cuda.set_device(args.device)
+
+    args.hidden_dim = 768 if 'base' in args.model_name else 384
+    print('hidden_dim:', args.hidden_dim)
+
+    # Load and prepare the model
+    model, compressed_model = get_models(args)
+
+    selected_layers = get_layer_names(args)
+
+    # extract original weights and input sequence lengths (batch size of weights)
+    original_weights, input_seq_lens = process_model_blocks(model, compressed_model, args, selected_layers=selected_layers, 
+                                                            skip_qkv=args.skip_qkv)
+
+    # Get data loaders
+    train_loader, val_loader = get_dataloaders_imagenet(args.batch_size)
+
+    encoders, decoders = get_encoder_decoders(selected_layers, input_seq_lens, args)
+    print('original_weights:', original_weights.keys())
+    print('encoder:', encoders)
+    print('decoder:', decoders)
+
+    analyze_parameter_storage(compressed_model, encoders, decoders, original_weights, args)
+
+    # Perform initial training steps to fit the decoder to the original weights
+    # initial_fit(encoders, decoders, original_weights, args)
+
+    # Create optimizer and scheduler
+    optimizer, scheduler, optimizer_ft, scheduler_ft = create_optimizer_scheduler(compressed_model, encoders, decoders, args)
+    
+    state_path = '/home/sahmed9/codes/DeepCompress-ViT/saved_models/base_rank_502/deit_base_patch16_224.pth'
+    state = torch.load(state_path)
+    print(state.keys())
+    
+    optimizer.load_state_dict(state['optimizer_state'])
+    scheduler.load_state_dict(state['scheduler_state'])
+    optimizer_ft.load_state_dict(state['optimizer_finetune_state'])
+    scheduler_ft.load_state_dict(state['scheduler_finetune_state'])
+    args.epochs = args.epochs - state['epoch']
+    print('Remaining epochs:', args.epochs)
+    
+    for key in encoders.keys():
+        encoders[key].load_state_dict(state['encoder_states'][key])
+        decoders[key].load_state_dict(state['decoder_states'][key])
+    
+    compressed_model.load_state_dict(state['model_state_dict'])
+        
+    # distilled model for knowledge distillation
+    if args.distilled_model:
+        del model
+        torch.cuda.empty_cache()
+        model = timm.create_model('deit_small_distilled_patch16_224', pretrained=True) if 'small' in args.model_name else timm.create_model('deit_base_distilled_patch16_224', pretrained=True)
+        model.to(args.device)
+        
+    # Train the model
+    best_acc = train(encoders, decoders, original_weights, model, compressed_model, train_loader, val_loader, 
+                     optimizer, scheduler, optimizer_ft, scheduler_ft, selected_layers, args)
+
+    print(f'Best accuracy: {best_acc:.2f}%')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train Vision Transformer with weight compression')
+    parser.add_argument('--model_name', type=str, default='deit_small_patch16_224', help='Name of the Vision Transformer model')
+    parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate for the Autoencoder')
+    parser.add_argument('--min-lr', type=float, default=0, help='Min. Learning rate for the Autoencoder')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train the Autoencoder')
+    parser.add_argument('--total_blocks', type=int, default=12, help='Number of blocks to compress')
+    parser.add_argument('--device', type=str, default='cuda:0', help='Device to use for training')
+    parser.add_argument('--initial_iters', type=int, default=1000, help='Number of initial iterations to train the Autoencoder')
+    parser.add_argument('--eval_interval', type=int, default=200000, help='Number of iterations to evaluate the model')
+    parser.add_argument('--mixed_precision', action='store_true', help='Use mixed precision training')
+    parser.add_argument('--batch_size', type=int, default=256, help='Batch size for ImageNet dataloader')
+    parser.add_argument('--base_dir', type=str, default='test', help='Base directory for saving models')
+    parser.add_argument('--skip_qkv', action='store_true', help='Skip compressing qkv layer in MultiheadAttention')
+    parser.add_argument('--mixup', action='store_true', help='Use Mixup')
+    parser.add_argument('--ce-weight', type=float, default=1.0, help='Weight for distillation loss')
+    parser.add_argument('--mse-weight', type=float, default=1.0, help='Weight for MSE loss')
+    parser.add_argument('--distillation-weight', type=float, default=3e3, help='Weight for distillation loss')
+    parser.add_argument('--finetune_other_params', action='store_true', help='Finetune other parameters of the model')
+    parser.add_argument('--opt', type=str, default='adamw', help='Optimizer to use for finetuning other parameters')
+    parser.add_argument('--weight-decay', type=float, default=0.05, help='weight decay (default: 0.05)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='SGD momentum (default: 0.9)')
+    parser.add_argument('--rank', type=int, default=276, help='Rank for encoded weight matrices')
+    parser.add_argument('--distilled_model', action='store_true', help='Use distilled model for distillation')
+    parser.add_argument('--warmup', action='store_true', help='Use warmup scheduler')
+    args = parser.parse_args()
+    args.finetune_other_params = True
+    args.distilled_model = True
+    # args.warmup = True
+    main(args)
